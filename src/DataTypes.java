3447fea739b49d60f4bc180a6d88aae530582be1
amir kayyal <amir.kayyal@gmail.com>
2023-08-08 13:28:40 +0330

    add a method for checking a character

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -7,11 +7,27 @@ public class DataTypes {
         int sum = add(num1, num2);
         System.out.println("sum = " + sum);
 
+        String s = "Hellow";
+        char c = s.charAt(0);
+        checkCharacter(c);
+
+
 
     }
 
     public static int add(int a , int b ){
         return a + b;
+    }
+    public static void checkCharacter(char c){
+        if (Character.isUpperCase(c)){
+            System.out.println("UpperCase Letter !");
+        } else if (Character.isLowerCase(c)) {
+            System.out.println("LowerCase Letter !");
+        } else if (Character.isDigit(c)) {
+            System.out.println("Digit");
+        }else{
+            System.out.println(c + "  = is a special character" );
+        }
     }
 
 }
