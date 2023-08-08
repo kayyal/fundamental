66895b851891d4f7d33942c5a8360b204e26c936
amir kayyal <amir.kayyal@gmail.com>
2023-08-08 13:05:56 +0330

    add a class for testing dataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int num1 = 10 ;
+        int num2 = 234;
+        int sum = add(num1, num2);
+        System.out.println("sum = " + sum);
+
+
+    }
+
+    public static int add(int a , int b ){
+        return a + b;
+    }
+
+}
