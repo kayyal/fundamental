505a2bffa654c8aac40318e50a108b37816ded7f
amir kayyal <amir.kayyal@gmail.com>
2023-08-12 11:23:30 +0330

    implement Market and Repository and Product

--- src/oop/market/Product.java
+++ src/oop/market/Product.java
@@ -1,4 +1,31 @@
 package oop.market;
 
 public class Product {
+    private String name;
+    private double price;
+    private double weight;
+    private boolean isShippable;
+
+    public Product(String name, double price, double weight, boolean isShippable) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+        this.isShippable = isShippable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public boolean isShippable() {
+        return isShippable;
+    }
 }
--- src/oop/market/ShoppingCart.java
+++ src/oop/market/ShoppingCart.java
@@ -1,4 +1,6 @@
 package oop.market;
 
 public class ShoppingCart {
+
+
 }
